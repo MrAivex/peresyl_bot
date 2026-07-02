faf66f083a8ebc8b6f26a7113949485cc8c8f822
GrafAleksey315 <lekstar_67@mail.ru>
2026-07-03 00:27:55 +0300

    10

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import yaml
 from orchestrator import Orchestrator
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
 )
 
